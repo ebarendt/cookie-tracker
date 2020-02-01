856112ea0a992e72165c6ca3cd447e0a606d301f
Eric Barendt <eric@barendt.org>
2020-02-01 08:19:52 -0800

    No online order entry

--- app/resources/order_download.rb
+++ app/resources/order_download.rb
@@ -7,12 +7,11 @@ class OrderDownload
   end
 
   def to_csv
-    headers = %w(girl_name) + Order::COOKIE_TYPES + Order::COOKIE_TYPES.map { |t| "#{t}_online" }
+    headers = %w(girl_name) + Order::COOKIE_TYPES
     CSV.generate(headers: headers, write_headers: true) do |csv|
       orders.each do |order|
         row = [order.girl_name]
         row.concat Order::COOKIE_TYPES.map { |t| order.public_send(t) }
-        row.concat Order::COOKIE_TYPES.map { |t| order.public_send("#{t}_online") }
         csv << row
       end
     end
